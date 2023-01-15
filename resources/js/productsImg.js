a0006262e29bde45500ba1aaadbcfc2556769e51
Carolina Martinez <caromartinezcorrea@gmail.com>
2023-01-15 03:59:00 -0300

    Modal con detalle de productos aplicado

---
+++ resources/js/productsImg.js
@@ -0,0 +1,44 @@
+let bigImg = document.querySelector('.big-img1 img');
+        function showImg1(pic){
+            bigImg.src = pic;
+        };
+
+let bigImg2 = document.querySelector('.big-img2 img');
+function showImg2(pic){
+    bigImg2.src = pic;
+}
+
+let bigImg3 = document.querySelector('.big-img3 img');
+function showImg3(pic){
+    bigImg3.src = pic;
+}
+
+let bigImg4 = document.querySelector('.big-img4 img');
+function showImg4(pic){
+    bigImg4.src = pic;
+}
+
+let bigImg5 = document.querySelector('.big-img5 img');
+function showImg5(pic){
+    bigImg5.src = pic;
+}
+
+let bigImg6 = document.querySelector('.big-img6 img');
+function showImg6(pic){
+    bigImg6.src = pic;
+}
+
+let bigImg7 = document.querySelector('.big-img7 img');
+function showImg7(pic){
+    bigImg7.src = pic;
+}
+
+let bigImg8 = document.querySelector('.big-img8 img');
+function showImg8(pic){
+    bigImg8.src = pic;
+}
+
+let bigImg9 = document.querySelector('.big-img9 img');
+function showImg9(pic){
+    bigImg9.src = pic;
+}
